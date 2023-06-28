4440af18d7398f01946678e8789c6981afd9f896
Navod-Shehan <2019cs196@stu.ucsc.lk>
2023-06-28 11:54:40 +0530

    admin dashboard okay

--- src/Admin/Dashboard/Dashboard.js
+++ src/Admin/Dashboard/Dashboard.js
@@ -1,7 +1,8 @@
-import { Table } from "antd";
+import { Button, Image, Space, Tooltip } from "antd";
 import { Col, Row, Card } from "antd";
 import React from "react";
 import {
+  AreaChart,
   BarChart,
   Bar,
   Cell,
@@ -10,74 +11,65 @@ import {
   ResponsiveContainer,
   PieChart,
   Pie,
+  CartesianGrid,
+  Area,
 } from "recharts";
-import userpic from "../../assets/images/user.png";
 import './Dashboard.css'
 import AdminNavbar from "../AdminNavbar/AdminNavbar";
+import { BankOutlined, CarryOutOutlined, CheckSquareOutlined, MedicineBoxOutlined, TeamOutlined } from "@ant-design/icons";
+import { Link } from "react-router-dom";
 
 const data1 = [
   {
-    name: "Page A",
+    name: "Western",
     uv: 4000,
     pv: 2400,
     amt: 2400,
   },
   {
-    name: "Page B",
+    name: "Southern",
     uv: 3000,
     pv: 1398,
     amt: 2210,
   },
   {
-    name: "Page C",
+    name: "Uva",
     uv: 2000,
     pv: 9800,
     amt: 2290,
   },
   {
-    name: "Page D",
+    name: "North Central",
     uv: 2780,
     pv: 3908,
     amt: 2000,
   },
   {
-    name: "Page E",
+    name: "Wayamba",
     uv: 1890,
     pv: 4800,
     amt: 2181,
   },
   {
-    name: "Page F",
+    name: "Sabaragamuwa",
     uv: 2390,
     pv: 3800,
     amt: 2500,
   },
   {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-  {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: "Page B",
+    name: "Central",
     uv: 3000,
     pv: 1398,
     amt: 2210,
   },
   {
-    name: "Page C",
+    name: "Eastern",
     uv: 2000,
     pv: 9800,
     amt: 2290,
   },
   {
-    name: "Page D",
+    name: "Northern",
     uv: 2780,
     pv: 3908,
     amt: 2000,
@@ -89,6 +81,75 @@ const data2 = [
   { name: "Group B", value: 300 },
   { name: "Group C", value: 300 },
   { name: "Group D", value: 200 },
+];
+
+const data3 = [
+  {
+    name: "New Born",
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: "1-2 Mon",
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: "4 Mon",
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: "6 Mon",
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: "7-11 Mon",
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: "12-23 Mon",
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: "2-3 Yr",
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: "4-6 Yr",
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: "7-10 Yr",
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: "11-12 Yr",
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: "13-18 Yr",
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
 ];
 
 const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
@@ -121,44 +182,6 @@ const renderCustomizedLabel = ({
   );
 };
 
-const columns = [
-  {
-    title: "Donor Name",
-    dataIndex: "DonorName",
-    sorter: {
-      compare: (a, b) => a.chinese - b.chinese,
-      multiple: 3,
-    },
-    className: "column1",
-  },
-  {
-    title: "Donation Date",
-    dataIndex: "Donation_Date",
-    sorter: {
-      compare: (a, b) => a.chinese - b.chinese,
-      multiple: 3,
-    },
-    className: "column2",
-  },
-  {
-    title: "Donate For",
-    dataIndex: "Donate_For",
-    sorter: {
-      compare: (a, b) => a.math - b.math,
-      multiple: 2,
-    },
-    className: "column3",
-  },
-  {
-    title: "Amount Rs.",
-    dataIndex: "Amount",
-    sorter: {
-      compare: (a, b) => a.english - b.english,
-      multiple: 1,
-    },
-    className: "column4",
-  },
-];
 
 const data = [
   {
@@ -191,9 +214,6 @@ const data = [
   },
 ];
 
-const onChange = (pagination, filters, sorter, extra) => {
-  console.log("params", pagination, filters, sorter, extra);
-};
 
 function getBarColor(index) {
   const colors = [
@@ -211,74 +231,196 @@ function getBarColor(index) {
 const Dashbaord = () => {
   return (
     <>
-      <AdminNavbar/>
-      <Row gutter={[16]} style={{paddingTop:'100px'}}>
+      <AdminNavbar />
+      <Row gutter={[32]} style={{ padding: '120px 20px 20px 20px' }}>
         <Col span={4}>
           <Card
-            title="Number of Registered Centres"
+          className="dashboard-cards"
             bordered={false}
-            style={{ backgroundColor: "#ff8042" }}
+            style={{ boxShadow: '0 2px 10px rgba(0, 0, 0, 0.2)'}}
           >
-            34
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+              <Space>
+                <BankOutlined style={{ fontSize: '20px' }} />
+                <span style={{ fontWeight: 'bolder' }}>Vaccination Centres</span>
+              </Space>
+            </div>
+            <Col span={24} style={{ textAlign: 'right', fontWeight: 'bolder' }}>
+              <span style={{ fontSize: '30px' }}>2234</span>
+            </Col>
           </Card>
         </Col>
         <Col span={4}>
           <Card
-            title="Number of Registered Parents"
+          className="dashboard-cards"
             bordered={false}
-            style={{ backgroundColor: "#e34d73" }}
+            style={{ boxShadow: '0 2px 10px rgba(0, 0, 0, 0.2)' }}
           >
-            200
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+              <Space>
+                <TeamOutlined style={{ fontSize: '20px' }} />
+                <span style={{ fontWeight: 'bolder' }}>Registered Parents</span>
+              </Space>
+            </div>
+            <Col span={24} style={{ textAlign: 'right', fontWeight: 'bolder' }}>
+              <span style={{ fontSize: '30px' }}>223</span>
+            </Col>
           </Card>
         </Col>
         <Col span={4}>
           <Card
-            title="Number of Registered Childrens"
+          className="dashboard-cards"
             bordered={false}
-            style={{ backgroundColor: "#82ca9d" }}
+            style={{ boxShadow: '0 2px 10px rgba(0, 0, 0, 0.2)' }}
           >
-            324
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+              <Space>
+                <TeamOutlined style={{ fontSize: '20px' }} />
+                <span style={{ fontWeight: 'bolder' }}>Registered Children</span>
+              </Space>
+            </div>
+            <Col span={24} style={{ textAlign: 'right', fontWeight: 'bolder' }}>
+              <span style={{ fontSize: '30px' }}>430</span>
+            </Col>
           </Card>
         </Col>
         <Col span={4}>
           <Card
-            title="Monthly Appointment"
+          className="dashboard-cards"
             bordered={false}
-            style={{ backgroundColor: "#ffbb28" }}
+            style={{ boxShadow: '0 2px 10px rgba(0, 0, 0, 0.2)' }}
           >
-            500
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+              <Space>
+                <CarryOutOutlined style={{ fontSize: '20px' }} />
+                <span style={{ fontWeight: 'bolder' }}>Monthly Appoinments</span>
+              </Space>
+            </div>
+            <Col span={24} style={{ textAlign: 'right', fontWeight: 'bolder' }}>
+              <span style={{ fontSize: '30px' }}>430</span>
+            </Col>
           </Card>
         </Col>
         <Col span={4}>
           <Card
-            title="Monthly Vaccines Administered"
+          className="dashboard-cards"
             bordered={false}
-            style={{ backgroundColor: "#00C49F" }}
+            style={{ boxShadow: '0 2px 10px rgba(0, 0, 0, 0.2)' }}
           >
-            743
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+              <Space>
+                <MedicineBoxOutlined style={{ fontSize: '20px' }} />
+                <span style={{ fontWeight: 'bolder' }}>Monthly Administered Vaccines</span>
+              </Space>
+            </div>
+            <Col span={24} style={{ textAlign: 'right', fontWeight: 'bolder' }}>
+              <span style={{ fontSize: '30px' }}>420</span>
+            </Col>
           </Card>
         </Col>
         <Col span={4}>
           <Card
-            title="Number of Vaccines Available"
+          className="dashboard-cards"
             bordered={false}
-            style={{ backgroundColor: "#a867b5" }}
+            style={{ boxShadow: '0 2px 10px rgba(0, 0, 0, 0.2)' }}
           >
-            543
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+              <Space>
+                <CheckSquareOutlined style={{ fontSize: '20px' }} />
+                <span style={{ fontWeight: 'bolder' }}>Available Vaccines</span>
+              </Space>
+            </div>
+            <Col span={24} style={{ textAlign: 'right', fontWeight: 'bolder' }}>
+              <span style={{ fontSize: '30px' }}>420</span>
+            </Col>
           </Card>
         </Col>
       </Row>
-      <Row gutter={[8]} style={{ marginTop: "30px", height: "900px" }}>
-        <Col span={12} className="who-receive-vaccines">
-          <Row>
+
+
+      <Row gutter={[32]} style={{ padding: '40px 20px 20px 20px' }}>
+        <Col span={12} className="donor-details">
+          <Row style={{ padding: '0px 50px  40px 0px' }}>
             <Col span={24}>
+              <Card type="inner" title="Donations" style={{ boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)', border: '2px solid grey' }} extra={<a href="#">More</a>}>
+                <Row >
+                  <Col span={8} style={{ textAlign: 'center', fontWeight: 'bolder' }}>
+                    <Row ><Col span={24}><span style={{ fontSize: '20px' }}>For Hospitals</span></Col></Row>
+                    <Row><Col span={24}><span style={{ fontSize: '30px' }}>220 000 LKR</span></Col></Row>
+                  </Col>
+                  <Col span={8} style={{ textAlign: 'center', fontWeight: 'bolder' }}>
+                    <Row ><Col span={24}><span style={{ fontSize: '20px' }}>For MOH</span></Col></Row>
+                    <Row><Col span={24}><span style={{ fontSize: '30px' }}>110 000 LKR</span></Col></Row>
+                  </Col>
+                  <Col span={8} style={{ textAlign: 'center', fontWeight: 'bolder' }}>
+                    <Row ><Col span={24}><span style={{ fontSize: '20px' }}>Total Donations</span></Col></Row>
+                    <Row><Col span={24}><span style={{ fontSize: '30px' }}>330 000 LKR</span></Col></Row>
+                  </Col>
+                </Row>
+              </Card>
+            </Col>
+          </Row>
+
+          <Row style={{ padding: '0px 50px  20px 0px' }}>
+            <Col span={24}>
+              <Card title="Reviews" style={{ boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)', border: '2px solid grey' }}>
+                <Row>
+                  <Col span={5} >
+                    <Row>
+                      <Col span={24}>
+                        <Image
+                          className="parentimage"
+                          width={60}
+                          src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+                        />
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col span={24}>
+                        <span style={{ fontSize: '16px', fontWeight: 'bolder' }}>Sandushi Jasinghe</span>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col span={10}>
+                        <span>26/07/2023</span>
+                      </Col>
+                      <Col span={2}></Col>
+                      <Col span={12}>
+                        <span>3.15 P.M</span>
+                      </Col>
+                    </Row>
+                  </Col>
+                  <Col span={19} style={{ paddingLeft: '15px' }}>
+                    <p className="description">
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne
+                      merninisti licere mihi ista probare, quae sunt a te dicta? Refert
+                      tamen, quo modo.Lorem ipsum dolor sit amet, consectetur adipiscing
+                      elit. Sed nonne merninisti licere mihi ista probare, quae sunt a te
+                      dicta? Refert tamen, quo modo. Lorem ipsum dolor sit amet, consectetur
+                    </p>
+                    <Row className="replybutton" style={{ paddingTop: '30px' }}>
+                      <Link to="/Reviews">
+                        <Button type="primary">More</Button>
+                      </Link>
+
+                    </Row>
+                  </Col>
+                </Row>
+              </Card>
+            </Col>
+          </Row>
+        </Col>
+
+        <Col span={12} >
+          <Row>
+            <Col span={24} style={{ padding: '0px 0px 30px 0px' }}>
               <h1 style={{ textAlign: "center" }}>Who Receive Vaccines</h1>
             </Col>
           </Row>
           <Row>
-            <Col span={24} style={{ height: "700px" }}>
+            <Col span={24} style={{ height: "350px" }}>
               <ResponsiveContainer width="100%" height="100%">
-                <BarChart width={150} height={40} data={data1}>
+                <BarChart width={150} height={40} data={data3}>
                   <XAxis dataKey="name" />
                   <YAxis />
                   <Bar dataKey="uv" fill="#8884d8" />
@@ -287,71 +429,45 @@ const Dashbaord = () => {
             </Col>
           </Row>
         </Col>
-        <Col span={12} className="donor-details">
-          <Card type="inner" title="Donations" extra={<a href="#">More</a>}>
-            <h3>For Hospitals:</h3>
-            <h3>For MOH:</h3>
-            <h3>Total Donations:</h3>
-          </Card><br/><br/>
+      </Row>
 
-          <Card title="Reports">
-            <Card
-              type="inner"
-              title="Lakshmi Jasinghe"
-              extra={<a href="#">More Reviews</a>}
-            >
+      <Row gutter={[32]} style={{ padding: '40px 20px 70px 20px' }}>
+        <Col span={12} className="donor-details">
+          <Row style={{ padding: '0px 50px  40px 0px' }}>
+            <Col span={24}>
               <Row>
-                <Row gutter={48}>
-                  <Col span={4}  className="userimage">
-                    {/* <h3>Parent ID</h3> */}
-                    <img
-                      src={userpic}
-                      alt="login"
-                      style={{ width: "40%", height: "60%" }}
-                    />
-                  </Col>
-                  <Col span={4}>
-                    <h3>Parent ID</h3>
-                    <p>P_1234 </p>
-                  </Col>
-                  <Col span={6}>
-                    <h3>To whom</h3>
-                    <p>Lady Ridgway Hospital  </p>
-                  </Col>
-                  <Col span={5}>
-                    <h3>Date </h3>
-                    <p>23rd september 2023 </p>
-                  </Col>
-                  <Col span={4}>
-                    <h3>Time</h3>
-                    <p>10.23 a.m</p>
-                  </Col>
-                </Row>
-                <Row>
                 <Col span={24}>
-              <h3 style={{ textAlign: "center" }}>Description</h3>
-              <p>TI recently visited XYZ Vaccination Centre to receive my COVID-19 vaccine, and I would like to share my review of the experience.Location and Facilities:XYZ Vaccination Centre is conveniently located in the heart of City XYZ. The centre is easily accessible by public transportation and offers ample parking space for those who prefer to drive. The facility is clean, well-maintained, and adheres to all necessary safety protocols. The waiting area is comfortable, with seating arrangements that allow for social distancing.</p>
-            </Col>
-                </Row>
+                  <h1 style={{ textAlign: "center" }}>Monthly Vaccines Count</h1>
+                </Col>
               </Row>
-            </Card>
-           
-          </Card>
+              <Row>
+                <Col span={24} style={{ height: "400px" }}>
+                  <ResponsiveContainer width="100%" height="100%">
+                    <AreaChart
+                      width={500}
+                      height={400}
+                      data={data1}
+                      margin={{
+                        top: 10,
+                        right: 30,
+                        left: 0,
+                        bottom: 0,
+                      }}
+                    >
+                      <CartesianGrid strokeDasharray="3 3" />
+                      <XAxis dataKey="name" />
+                      <YAxis />
+                      <Tooltip />
+                      <Area type="monotone" dataKey="uv" stroke="#8884d8" fill="#8884d8" />
+                    </AreaChart>
+                  </ResponsiveContainer>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
         </Col>
-      </Row>
-      <Row style={{ height: "500px" }}>
-        <Col span={24}>
-          <Table
-            columns={columns}
-            dataSource={data}
-            onChange={onChange}
-            pagination={false}
-            className="table-container"
-          />
-        </Col>
-      </Row>
-      <Row gutter={[8]} style={{ marginTop: "30px", height: "500px" }}>
-        <Col span={12} className="issued-vaccines">
+
+        <Col span={12} style={{ boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)', border: '2px solid grey' }}>
           <Row>
             <Col span={24}>
               <h1 style={{ textAlign: "center" }}>Iassued Vaccines</h1>
@@ -379,28 +495,6 @@ const Dashbaord = () => {
                     ))}
                   </Pie>
                 </PieChart>
-              </ResponsiveContainer>
-            </Col>
-          </Row>
-        </Col>
-        <Col span={12} className="monthly-vaccine-count">
-          <Row>
-            <Col span={24}>
-              <h1 style={{ textAlign: "center" }}>Monthly Vaccines Count</h1>
-            </Col>
-          </Row>
-          <Row>
-            <Col span={24} style={{ height: "400px" }}>
-              <ResponsiveContainer width="100%" height="100%">
-                <BarChart width={150} height={40} data={data1}>
-                  <XAxis dataKey="name" />
-                  <YAxis />
-                  <Bar dataKey="uv" fill="#ebb594" />
-
-                  {data.map((entry, index) => (
-                    <Cell key={`cell-${index}`} fill={getBarColor(index)} />
-                  ))}
-                </BarChart>
               </ResponsiveContainer>
             </Col>
           </Row>

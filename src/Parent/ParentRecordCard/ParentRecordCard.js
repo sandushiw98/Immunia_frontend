5b27b651d3834b1c02a1ee992dfdfb10ec84f0ca
sandushiw98 <86320767+sandushiw98@users.noreply.github.com>
2023-07-09 02:14:08 +0530

    child account modifications part 2

--- src/Parent/ParentRecordCard/ParentRecordCard.js
+++ src/Parent/ParentRecordCard/ParentRecordCard.js
@@ -1,9 +1,13 @@
 import React from "react";
 import "./ParentRecordCard.css";
 import ParentNavbar from "../ParentNavbar/ParentNavbar";
-import { Link } from 'react-router-dom';
-import image1 from "../../assets/images/vaccine-record-removebg-preview.png";
+import { Link } from "react-router-dom";
+import { DownloadOutlined } from "@ant-design/icons";
+import image1 from "../../assets/images/reminder.jpg";
 import { DownOutlined, UserOutlined } from "@ant-design/icons";
+
+import { useState } from "react";
+
 import {
   Avatar,
   Col,
@@ -18,25 +22,76 @@ import {
   Input,
 } from "antd";
 import image2 from "../../assets/images/childprofile3.jpg";
+import { PureComponent } from "react";
 import {
-  EditOutlined,
-  EllipsisOutlined,
-  SettingOutlined,
-} from "@ant-design/icons";
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
 
 const { Meta } = Card;
 
+const dataweight = [
+  {
+    name: "Page A",
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: "Page B",
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: "Page C",
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: "Page D",
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: "Page E",
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: "Page F",
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: "Page G",
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
+
 const columns = [
-  
   {
     title: "Vaccine Type",
     dataIndex: "vaccine",
+  
   },
   {
     title: "Dosage",
     dataIndex: "dosage",
   },
-  
+
   {
     title: "Location",
     dataIndex: "location",
@@ -63,11 +118,12 @@ const data = [
     location: "Lady Ridgway",
   },
 
-  { key: "3",
-  date: "3/5/2022",
-  dosage: "3rd dose",
-  vaccine: "Polio",
-  location: "Lady Ridgway",
+  {
+    key: "3",
+    date: "3/5/2022",
+    dosage: "3rd dose",
+    vaccine: "Polio",
+    location: "Lady Ridgway",
   },
   {
     key: "4",
@@ -76,7 +132,6 @@ const data = [
     vaccine: "Sarampa",
     location: "National Hospital",
   },
-  
 ];
 
 const handleButtonClick = (e) => {
@@ -89,7 +144,6 @@ const handleMenuClick = (e) => {
   console.log("click", e);
 };
 
-
 const data1 = [
   {
     key: "1",
@@ -101,112 +155,110 @@ const data1 = [
 ];
 
 const menuProps = {
-  
   onClick: handleMenuClick,
 };
 
 const columnschild = [
- 
   {
-    title: 'Vaccine',
-    dataIndex: 'vaccine',
-    key: 'vaccine',
-    width: 80,
+    title: <span class="vaccine_details">Vaccine</span>,
+    dataIndex: "vaccine",
+    key: "vaccine",
   },
   {
-    title: 'Dosage',
-    dataIndex: 'Dosage',
-    key: 'Dosage',
-    
+    title: <span class="vaccine_details">Dosage</span>,
+    dataIndex: "Dosage",
+    key: "Dosage",
   },
   {
-    title: 'Return Date',
-    dataIndex: 'Return_Date',
-    key: 'Return_Date',
-    
+    title: <span class="vaccine_details">Return Date</span>,
+    dataIndex: "Return_Date",
+    key: "Return_Date",
   },
-  
-  
 ];
 const datachild = [
   {
-    key: '1',
-    vaccine: 'Sarampa',
-    Dosage: '2nd Dose',
-    Return_Date: '02/08/2022',
+    key: "1",
+    vaccine: <span class="return">Sarampa</span>,
+    Dosage: <span class="return">2nd Dose</span>,
+    Return_Date: <span class="return">02/08/2022</span>,
   },
- 
 ];
 
-
 const ParentRecordCard = () => {
+  const [size, setSize] = useState("large");
+
   return (
     <div className="cover">
       <Row>
         <ParentNavbar />
       </Row>
-      <Row style={{paddingTop: '120px', paddingRight: '30px', display: 'flex', justifyContent: 'right', alignItems: 'right'}} >
-      <Link  to='../../ParentSearch' underline="none"><Button type="primary" > Create New + </Button> </Link>
+      <Row
+        style={{
+          paddingTop: "120px",
+          paddingRight: "30px",
+          display: "flex",
+          justifyContent: "right",
+          alignItems: "right",
+        }}
+      >
+        <Link to="../../ParentChildAccount" underline="none">
+          <Button type="primary"> Create New + </Button>{" "}
+        </Link>
       </Row>
       <Row>
         <Col span={10} style={{ paddingTop: "60px", paddingLeft: "150px" }}>
           <Card
             style={{
-              width: 500,
+              width: 400,
             }}
             cover={<img alt="example" src={image2} />}
-            actions={[
-              <SettingOutlined key="setting" />,
-              <EditOutlined key="edit" />,
-              <EllipsisOutlined key="ellipsis" />,
-            ]}
           >
             <Meta
               avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
-              title="Amani Wirathma Wimalaweera "
-              //   description="Age  : 1 year"
+              title=<span className="custom-table-header">
+                Amani Wirathma Wimalaweera
+              </span>
             />
           </Card>
         </Col>
         <Col span={14} className="childtable">
           <Card className="childshadow">
             <div className="childcontent">
-          <table class="childclass">
-          <tr>
-              <th>Vaccination Card ID:</th>
-              <td>C345</td>
-            </tr>
-            <tr>
-              <th>Age:</th>
-              <td>1 Year</td>
-            </tr>
-            <tr>
-              <th>Weight:</th>
-              <td>5kg 400g</td>
-            </tr>
-            <tr>
-              <th>Date Of Birth:</th>
-              <td> 2022/ 07/ 08</td>
-            </tr>
-            <tr>
-              <th>Address:</th>
-              <td> Sulakshi, Temple Road, Colombo 8</td>
-            </tr>
-            <tr>
-              <th>Gender:</th>
-              <td>Female</td>
-            </tr>
-            <tr>
-              <th>Guardian Name:</th>
-              <td> Lakshmi Jasinghe</td>
-            </tr>
-            <tr>
-              <th>Contact Number:</th>
-              <td> 071-4020626</td>
-            </tr>
-            
-          </table>
-          </div>
+              <table class="childclass">
+                <tr>
+                  <th>Vaccination Card ID:</th>
+                  <td>C345</td>
+                </tr>
+                <tr>
+                  <th>Age:</th>
+                  <td>1 Year</td>
+                </tr>
+                <tr>
+                  <th>Weight:</th>
+                  <td>5kg 400g</td>
+                </tr>
+                <tr>
+                  <th>Date Of Birth:</th>
+                  <td> 2022/ 07/ 08</td>
+                </tr>
+                <tr>
+                  <th>Address:</th>
+                  <td> Sulakshi, Temple Road, Colombo 8</td>
+                </tr>
+                <tr>
+                  <th>Gender:</th>
+                  <td>Female</td>
+                </tr>
+                <tr>
+                  <th>Guardian Name:</th>
+                  <td> Lakshmi Jasinghe</td>
+                </tr>
+                <tr>
+                  <th>Contact Number:</th>
+                  <td> 071-4020626</td>
+                </tr>
+              </table>
+            </div>
           </Card>
         </Col>
       </Row>
@@ -221,59 +273,122 @@ const ParentRecordCard = () => {
         <h1>Child Vaccination Record Card</h1>
       </Row>
 
-      
       <Row>
         <Table
           columns={columns}
           dataSource={data}
           style={{ width: 2000, padding: "30px" }}
-          pagination = {false}
+          pagination={false}
+         
         />
       </Row>
       <Row>
-        <h2 style={{paddingLeft: '30px'}}>Return Date</h2>
+        <h2 style={{ paddingLeft: "30px" }}>Return Date</h2>
       </Row>
       <Row className="returndate">
-      <Table columns={columnschild} dataSource={datachild} pagination= {false} style={{width: '100%', paddingLeft: '20px', paddingRight: '20px',color: 'red'}} />;
+        <Table
+          columns={columnschild}
+          dataSource={datachild}
+          pagination={false}
+          style={{
+            width: "100%",
+            paddingLeft: "20px",
+            paddingRight: "20px",
+            color: "red",
+          }}
+        />
+        ;
       </Row>
-      
+      <Row
+        style={{
+          display: "flex",
+          alignContent: "right",
+          justifyContent: "right",
+          paddingRight: "30px",
+          paddingTop: "30px",
+          paddingBottom: "30px",
+        }}
+      >
+        <Button type="primary" icon={<DownloadOutlined />} size={size}>
+          Download
+        </Button>
+      </Row>
+
       <Row className="reminder">
-        <div class="notifications-container">
-          <div class="alert">
-            <div class="flex">
-              <div class="flex-shrink-0">
-                <svg
-                  aria-hidden="true"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="h-5 w-5 alert-svg"
-                >
-                  <path
-                    clip-rule="evenodd"
-                    d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z"
-                    fill-rule="evenodd"
-                  ></path>
-                </svg>
-              </div>
-              <div class="alert-prompt-wrap">
-                <h1>REMINDER</h1>
-                <p class="text-sm text-yellow-700">
-                Please keep this record card, it includes the medical information,
-          details and the vaccine you have received. This card will show the
-          next schedule of your vaccine. It is important to show this card to
-          the next vaccination schedule for health officials to verify.
-                  {/* <a class="alert-prompt-link" href="#">
+        <Col span={10} style={{ paddingLeft: "50px" }}>
+          <img
+            src={image1}
+            alt="girl"
+            style={{ width: "70%", height: "70%", position: "fix" }}
+          />
+        </Col>
+        <Col span={14}>
+          <div class="notifications-container">
+            <div class="alert">
+              <div class="flex">
+                <div class="flex-shrink-0">
+                  <svg
+                    aria-hidden="true"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-5 w-5 alert-svg"
+                  >
+                    <path
+                      clip-rule="evenodd"
+                      d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z"
+                      fill-rule="evenodd"
+                    ></path>
+                  </svg>
+                </div>
+
+                <div class="alert-prompt-wrap">
+                  <h1>REMINDER</h1>
+                  <p class="text-sm text-yellow-700">
+                    Please keep this record card, it includes the medical
+                    information, details and the vaccine you have received. This
+                    card will show the next schedule of your vaccine. It is
+                    important to show this card to the next vaccination schedule
+                    for health officials to verify.
+                    {/* <a class="alert-prompt-link" href="#">
                     Upgrade To Premium
                   </a> */}
-                </p>
+                  </p>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        </Col>
       </Row>
-      
-      
+
+      <Row style={{ paddingTop: "50px" }}>
+        <ResponsiveContainer width="100%" height="100%">
+          <LineChart
+            width={500}
+            height={300}
+            data={dataweight}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line
+              type="monotone"
+              dataKey="pv"
+              stroke="#8884d8"
+              activeDot={{ r: 8 }}
+            />
+            <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+          </LineChart>
+        </ResponsiveContainer>
+      </Row>
     </div>
   );
 };
